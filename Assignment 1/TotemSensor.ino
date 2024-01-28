e61f6942ec5c09ba60010ebca6ea36cf7f41ae22
Felix Wallin <60824607+felwal@users.noreply.github.com>
2024-01-28 22:20:21 +0100

    Map totems -> color dynamically

--- Assignment 1/TotemSensor.ino
+++ Assignment 1/TotemSensor.ino
@@ -2,11 +2,12 @@ const int PIN_IN_TOTEM_R = 0;
 const int PIN_IN_TOTEM_G = 1;
 const int PIN_IN_TOTEM_B = 2;
 
-const int TOTEM_COUNT = 9;
+const int MAX_HEIGHT = 3;
+const int TOTEM_COUNT = MAX_HEIGHT * 3;
 
 const int VOLT_IN = 5;
 const int RES_FIXED = 1;
-const int RES_TOTEM = 2; // should be 2x FIXED
+const int RES_TOTEM = 2; // should be 2x FIXED for widest span
 
 void setup() {
   Serial.begin(9600);
@@ -23,15 +24,12 @@ void loop() {
   int totemsG = totemRead(PIN_IN_TOTEM_G);
   int totemsB = totemRead(PIN_IN_TOTEM_B);
 
-  // -> 0, 5, A, F
-  int r = toHexLetter(totemsR);
-  int g = toHexLetter(totemsG);
-  int b = toHexLetter(totemsB);
+  int r = toRgbVal(totemsR);
+  int g = toRgbVal(totemsG);
+  int b = toRgbVal(totemsB);
 
-  String hex = "#" + (r + r) + (g + g) + (b + b);
-
-  // send to Processing
-  //Serial.println(hex)
+  String hex = "#" + hexify(r) + hexify(g) + hexify(b);
+  Serial.println(hex);
 }
 
 void connectProcessing() {
@@ -56,7 +54,7 @@ int totemRead(int pin) {
 
   //Serial.println("inVal: " + inVal);
 
-  for (int totems = 0; totems <= TOTEM_COUNT; totems++) {
+  for (int totems = 0; totems <= MAX_HEIGHT; totems++) {
     int resistors = totems + 1; // one in the base
     int resTot = RES_TOTEM / resistors;
     int voltOut = VOLT_IN * (resTot / (resTot + RES_FIXED));
@@ -76,17 +74,18 @@ int totemRead(int pin) {
     }
   }
 
-  return -1;
+  return MAX_HEIGHT;
 }
 
-String toHexLetter(int totemCount) {
-  // TODO: dynamic totem count / mapping
+int toRgbVal(int totems) {
+  // +1 for empty pole
+  int valPerTotem = 256 / (MAX_HEIGHT + 1);
 
-  switch (totemCount) {
-    case 0: return "0";
-    case 1: return "5";
-    case 2: return "A";
-    case 3: return "F";
-    default: return "F"; // TODO: some odd color
-  }
+  return totems * valPerTotem - totems == 0 ? 0 : 1;
+}
+
+String hexify(int dec) {
+  // make sure it is of length 2
+  String hex = String(dec, HEX);
+  return hex.length() == 2 ? hex : "0" + hex;
 }
